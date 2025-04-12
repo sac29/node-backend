b92dfe44725858fe1991aab4af9198c5a91451d2
Sachin <sachin.vish01@gmail.com>
2025-04-12 21:51:25 +0530

    added change password route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    getUserChannelProfile
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,5 +32,6 @@ router.route("/refresh-token").post(refreshAccessToken);
 //secured routes
 
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
 
 export default router;
